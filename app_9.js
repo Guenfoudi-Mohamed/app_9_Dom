28d550ba2615d29243e8b65008713cb28259afe8
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-04-05 19:44:14 +0100

    Update app_9.js

--- app_9.js
+++ app_9.js
@@ -21,6 +21,24 @@ function clearEvent(index){
     let num = box[index].children[1].children[2].children.length;
     for(let i = 0;i<num;i++){
         box[index].children[1].children[2].children[i].style.cssText = `color:transparent;`;
+        box[index].children[1].children[2].children[i].addEventListener('mouseover',function(){mouseover(index,i)});
+        box[index].children[1].children[2].children[i].addEventListener('mouseout',function(){mouseout(index,i)});
+        box[index].children[1].children[2].children[i].addEventListener('click',function(){mouseclick(index,i)});
+    };
+    function mouseover(index,i){
+        for(let x = 0;x<num;x++){
+            box[index].children[1].children[2].children[x].style.cssText = `color:transparent;`;
+        };
+    };
+    function mouseout(index,i){
+        for(let x = 0;x<num;x++){
+            box[index].children[1].children[2].children[x].style.cssText = `color:transparent;`;
+        };  
+    };
+    function mouseclick(index,i){
+        for(let x = 0;x<num;x++){
+            box[index].children[1].children[2].children[x].style.cssText = `color:transparent;`;
+        };  
     };
 }
 
@@ -28,7 +46,7 @@ function addEvent(index){
     let num = box[index].children[1].children[2].children.length;
     for(let i = 0;i<num;i++){
         box[index].children[1].children[2].children[i].addEventListener('mouseover',function(){mouseover(index,i)});
-        // box[index].children[1].children[2].children[i].addEventListener('mouseout',function(){mouseout(index,i)});
+        box[index].children[1].children[2].children[i].addEventListener('mouseout',function(){mouseout(index,i)});
         box[index].children[1].children[2].children[i].addEventListener('click',function(){mouseclick(index,i)});
     };
     function mouseover(index,i){
@@ -42,15 +60,24 @@ function addEvent(index){
         };  
     };
     function mouseclick(index,i){
-        for(let x = 0;x<=i;x++){
-            box[index].children[1].children[2].children[x].style.cssText = `color:rgb(153, 34, 233);`;
-        };
-        for(let u = i+1;u<num;u++){
-            box[index].children[1].children[2].children[u].style.cssText = `color:transparent;`;
-        }
         for(let x = 0;x<num;x++){
-            box[index].children[1].children[2].children[x].removeEventListener('mouseover',mouseover);
-            box[index].children[1].children[2].children[x].removeEventListener('mouseout',mouseout);
+            if(x<=i){
+                box[index].children[1].children[2].children[x].style.cssText = `color:rgb(153, 34, 233);`;
+                continue;
+            }
+            box[index].children[1].children[2].children[x].style.cssText = `color:transparent`;
+        };
+        for(let x = 0;x<num;x++){
+            box[index].children[1].children[2].children[x].addEventListener('mouseout',function(){
+                for(let x=0;x<num;x++){
+                    if(x<=i){
+                        box[index].children[1].children[2].children[x].style.cssText = `color:rgb(153, 34, 233);`;
+                    }
+                    else{
+                        box[index].children[1].children[2].children[x].style.cssText = `color:transparent`;
+                    }
+                }
+            });
         }
     };
 };
